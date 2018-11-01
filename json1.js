7f3dab793d71200c681ccb491917f130d61986aa
dibartolo-mallory <dib17003@byui.edu>
2018-11-01 12:35:35 -1000

    .

--- json1.js
+++ json1.js
@@ -1,4 +1,4 @@
-var xmlhttp = new XMLHttpRequest();
+/*var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
@@ -6,4 +6,15 @@ xmlhttp.onreadystatechange = function() {
     }
 };
 xmlhttp.open("GET", "/json.json", true);
+xmlhttp.send();*/
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("demo").innerHTML = myObj.message;
+    }
+};
+xmlhttp.open("GET", "https://dog.ceo/api/breeds/list/all
+", true);
 xmlhttp.send();
