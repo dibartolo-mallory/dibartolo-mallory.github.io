2c3e131adbc2b663a37bcc60a5d71bd8202bea8b
dibartolo-mallory <dib17003@byui.edu>
2018-11-01 18:19:36 -1000

    .

--- json1.js
+++ json1.js
@@ -1,8 +1,18 @@
+/*var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("demo").innerHTML = myObj.name; 
+    }
+};
+xmlhttp.open("GET", "/json.json", true);
+xmlhttp.send();*/
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.name; //myObj.pets[0].animal;
+        document.getElementById("demo").innerHTML = myObj.name;
     }
 };
 xmlhttp.open("GET", "/json.json", true);
