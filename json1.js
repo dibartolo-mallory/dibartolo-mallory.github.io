3d2feb236c968bd55599025e38e17ba2f8986729
dibartolo-mallory <dib17003@byui.edu>
2018-11-01 18:15:46 -1000

    .

--- json1.js
+++ json1.js
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.pets[0].animal;
+        document.getElementById("demo").innerHTML = myObj.name; //myObj.pets[0].animal;
     }
 };
 xmlhttp.open("GET", "/json.json", true);
