42e9ab90279cce12ff9bf35a62b16d999cc6ad00
dibartolo-mallory <dib17003@byui.edu>
2018-11-01 18:11:17 -1000

    .

--- json1.js
+++ json1.js
@@ -1,20 +1,10 @@
-/*var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.name;
-    }
-};
-xmlhttp.open("GET", "/json.json", true);
-xmlhttp.send();*/
-
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.message[0];
+        document.getElementById("demo").innerHTML = myObj.pets[0].animal;
     }
 };
-xmlhttp.open("GET", "https://dog.ceo/api/breeds/list/all
-", true);
+xmlhttp.open("GET", "/json.json", true);
 xmlhttp.send();
+
