d74327ec6be29b911a42c36fc995cb7e5d3e0fe3
dibartolo-mallory <dib17003@byui.edu>
2018-11-17 14:31:16 -1000

    Updated css-class files

--- css-class.js
+++ css-class.js
@@ -1,3 +1,4 @@
+/*Slider script*/
 function toggleNav() {
        var updateElement = document.getElementById("menu-icon");
        var showMenu = document.getElementById("menu");
@@ -5,3 +6,58 @@ function toggleNav() {
     updateElement.classList.toggle("slide-button");
     showMenu.classList.toggle("slide-menu"); 
 }  
+/*Mouse Over Script*/
+function addClass(x) {
+  x.classList.add("largerCircle"); 
+}
+
+function removeClass() {
+  x.classList.remove("largerCircle"); 
+}
+
+/*On Change Script*/
+function onChange() {
+  var button = document.getElementById("submitButton"); 
+  button.classList.add("highlight"); 
+  
+}
+
+/*On Focus Script*/
+function onFocus() {
+   input = document.getElementById("inputField");
+  input.classList.add("inputHighlight");
+
+}
+
+function onBlur() {
+  var input = document.getElementById("inputField");
+  input.classList.remove("inputHighlight");
+}
+
+function check() {
+  var inputField = document.getElementById("inputField2"); 
+  var check = document.getElementById("inputField2").value == "blue"; 
+  if(check) {
+      inputField.classList.remove("incorrectValue"); 
+      inputField.classList.toggle("correctValue");
+  }else {
+    inputField.classList.remove("correctValue"); 
+    inputField.classList.toggle("incorrectValue"); 
+  }
+  
+  
+}
+
+
+// Touch triggers
+const touchEvents = document.getElementById("canvas");
+touchEvents.addEventListener("touchstart", touchStart);
+touchEvents.addEventListener("touchend", touchEnd);
+
+function touchStart() {
+    touchEvents.classList.toggle('touchBackgroundAnimation');
+}
+
+function touchEnd() {
+    touchEvents.classList.toggle('touchBackgroundAnimation'); 
+}
